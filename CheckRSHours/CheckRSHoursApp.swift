b52257aff475fd82d67e55b2a00c4c4179f7d05e
ilabrosimov <ilabrosimov@gmail.com>
2021-12-25 20:37:25 +0300

    Initial Commit

---
+++ CheckRSHours/CheckRSHoursApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckRSHoursApp.swift
+//  CheckRSHours
+//
+//  Created by ilabrosimov on 25.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct CheckRSHoursApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
